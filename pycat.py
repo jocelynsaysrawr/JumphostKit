44ffdff853388e5ccb12acbe32bec51c9ec1238f
Jocelyn Belden <jocebelden@gmail.com>
2018-12-01 16:04:47 -1000

    test

--- pycat.py
+++ pycat.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+import sys
+import socket
+import argparse
+import threading
+import subprocess
+
+# global variables
+listen = False
+command = False
+upload = False
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
+
 def main():
     print("I'm in main!!!")
 
